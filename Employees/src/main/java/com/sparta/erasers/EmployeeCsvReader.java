f15e1cf11fdad8c6badf0acf1cc9fdd3d12ed311
Tom Allen <TAllen@spartaglobal.com>
2024-03-25 16:32:15 +0000

    Added readFileLines method body

--- Employees/src/main/java/com/sparta/erasers/EmployeeCsvReader.java
+++ Employees/src/main/java/com/sparta/erasers/EmployeeCsvReader.java
@@ -15,18 +15,19 @@ public class EmployeeCsvReader {
 
         Path input = Paths.get("src/main/java/com/sparta/erasers/employees_short.csv");
         Path output = Paths.get("src/main/java/com/sparta/erasers/output.txt");
-
+        EmployeeCsvReader csvReader = new EmployeeCsvReader();
         BufferedReader reader = null;
         try {
             reader = Files.newBufferedReader(input);
             //System.out.println(reader.readLine());
             System.out.println(reader.readLine());
-            EmployeeCsvReader csvReader = new EmployeeCsvReader();
+
             csvReader.createEmployee(reader.readLine());
             System.out.println(csvReader.createEmployee(reader.readLine()).getEmail());
         } catch (IOException e) {
             System.out.println("Not found.");
         }
+        System.out.println(csvReader.readFileLines("employees_short.csv").size());
 
 
     }
@@ -43,7 +44,23 @@ public class EmployeeCsvReader {
 
 
     // opens and reads a file and returns a ArrayList of Strings, one for each line
-//    public ArrayList<String>  readFileLines(String fileName) {...}
+    public ArrayList<String>  readFileLines(String fileName) {
+        ArrayList<String> fileLines = new ArrayList<>();
+        String filePath = "src/main/java/com/sparta/erasers/" + fileName;
+        Path input = Paths.get(filePath);
+        BufferedReader reader = null;
+
+        try {
+            reader = Files.newBufferedReader(input);
+            reader.skip(107);
+            for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+                fileLines.add(line);
+            }
+        } catch (IOException e) {
+            System.out.println("File not found.");
+        }
+        return fileLines;
+    }
 
 
     // creates an ArrayList of Employee objects from information stored in a .csv file.
