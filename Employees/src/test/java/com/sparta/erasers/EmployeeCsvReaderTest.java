5430cc838b7570b7eb355bd176e5895192b85382
Mohammad Zafar <mzcoder7@gmail.com>
2024-03-28 15:20:09 +0000

    Testing 28 03 (#6)
    * Employee unit tests fully implemented
    
    * Employee CSV Reader unit tests fully implemented
    
    ---------
    
    Co-authored-by: DiarmuidJackHealy <DHealy@spartaglobal.co.uk>

--- Employees/src/test/java/com/sparta/erasers/EmployeeCsvReaderTest.java
+++ Employees/src/test/java/com/sparta/erasers/EmployeeCsvReaderTest.java
@@ -9,14 +9,46 @@ import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeCsvReaderTest {
-
-
-
     @Test
-    void readFileLines() {
+    void createEmployee() {
+        EmployeeCsvReader reader = new EmployeeCsvReader();
+        Assertions.assertNotNull(reader.createEmployee("198429,Mrs.,Serafina,I,Bumgarner,F,serafina.bumgarner@exxonmobil.com,9/21/1982,02/01/2008,69294"));
     }
 
     @Test
-    void readEmployees() {
+    void testReadFileLines() {
+        EmployeeCsvReader reader = new EmployeeCsvReader();
+        Assertions.assertEquals(reader.readFileLines("employees_short.csv").size(), 10);
+    }
+
+    @Test
+    void testReadFirstFileLineObject() {
+        EmployeeCsvReader reader = new EmployeeCsvReader();
+        Assertions.assertEquals(reader.readFileLines("employees_short.csv").get(0), "198429,Mrs.,Serafina,I,Bumgarner,F,serafina.bumgarner@exxonmobil.com,9/21/1982,02/01/2008,69294");
+    }
+
+    @Test
+    void testReadLastFileLineObject() {
+        EmployeeCsvReader reader = new EmployeeCsvReader();
+        Assertions.assertEquals(reader.readFileLines("employees_short.csv").get(reader.readEmployees("employees_short.csv").size()-1), "744723,Hon.,Bibi,H,Paddock,F,bibi.paddock@yahoo.co.in,10/20/1991,11/02/2016,87148");
+
+    }
+
+    @Test
+    void testReadEmployees() {
+        EmployeeCsvReader reader = new EmployeeCsvReader();
+        Assertions.assertEquals(reader.readEmployees("employees_short.csv").size(), 10);
+    }
+
+    @Test
+    void testReadFirstEmployeeObject() {
+        EmployeeCsvReader reader = new EmployeeCsvReader();
+        Assertions.assertEquals(reader.readEmployees("employees_short.csv").get(0).getEmp_no(), 198429);
+    }
+
+    @Test
+    void testReadLastEmployeeObject() {
+        EmployeeCsvReader reader = new EmployeeCsvReader();
+        Assertions.assertEquals(reader.readEmployees("employees_short.csv").get(reader.readEmployees("employees_short.csv").size()-1).getEmp_no(), 744723);
     }
 }
